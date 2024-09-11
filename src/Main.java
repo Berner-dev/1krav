e59b90e34b759489858f121d16c0cb762188c192
Casper <thexnetworkop@gmail.com>
2024-09-11 14:59:19 +0200

    Changes to formatting

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,17 @@ public class Main {
 
         //Indtastet oplysninger fra tid på dagen, trafikmængden, områdets type og om det er en nødsituation.
 
-        System.out.print ("Er det dag, aften eller nat?");
-        String tidpaadagen = scanner.nextLine(). toLowerCase();
+        System.out.print("Er det dag, aften eller nat?");
 
-        System.out.print ("Er trafikmængden høj eller lav?");
-        String trafikmaengde = scanner.nextLine(). toLowerCase();
+        String tidpaadagen = scanner.nextLine().toLowerCase();
+        System.out.print("Er trafikmængden høj eller lav?");
+        String trafikmaengde = scanner.nextLine().toLowerCase();
 
-        System.out.print ("Er det et boligområde, erhvervsområde eller blandet område?");
-        String omraadetstype = scanner.nextLine(). toLowerCase();
+        System.out.print("Er det et boligområde, erhvervsområde eller blandet område?");
+        String omraadetstype = scanner.nextLine().toLowerCase();
 
-        System.out.print ("Er det en nødsituation´?");
-        String nødtilstand = scanner.nextLine(). toLowerCase();
-
-        //test//
-
-        //
-
-
-
-
-
-
-
-
+        System.out.print("Er det en nødsituation´?");
+        String nødtilstand = scanner.nextLine().toLowerCase();
 
 
     }
