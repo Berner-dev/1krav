9c8dd018e585cc5b73945adfae96273c45c752a1
Oliver, Berner <oliverbc321@gmail.com>
2024-09-11 14:33:15 +0200

    Krav 1 opgave

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Indtastet oplysninger fra tid på dagen, trafikmængden, områdets type og om det er en nødsituation.
+
+        System.out.print ("Er det dag, aften eller nat?");
+        String tidpaadagen = scanner.nextLine(). toLowerCase();
+
+        System.out.print ("Er trafikmængden høj eller lav?");
+        String trafikmaengde = scanner.nextLine(). toLowerCase();
+
+        System.out.print ("Er det et boligområde, erhvervsområde eller blandet område?");
+        String omraadetstype = scanner.nextLine(). toLowerCase();
+
+        System.out.print ("Er det en nødsituation´?");
+        String nødtilstand = scanner.nextLine(). toLowerCase();
+
+        //
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
